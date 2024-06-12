8ab9580603182148d7af2ebede0c00d03f332e22
kysniw <kuba.winsyk.forgames@gmail.com>
2024-06-12 15:24:42 +0200

    Working with styling header.

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -42,12 +42,12 @@ const Header = () => {
 
   return (
     <section
-      className="min-h-screen md:h-screen flex flex-col md:flex-row justify-center items-center
-     gap-20 md:px-12 overflow-hidden snap-start"
+      className="min-h-screen lg:h-screen flex flex-col lg:flex-row justify-center items-center
+     gap-20 lg:px-12 overflow-hidden snap-start relative"
     >
       <div
-        className="flex flex-col max-w-[1100px] text-center h-full justify-evenly
-       px-12 md:px-40 relative"
+        className="flex flex-col max-w-[1100px] text-center min-h-screen justify-evenly
+       px-12 lg:px-40 z-10 bg-black bg-opacity-60 lg:bg-transparent"
       >
         <div>
           <h1 className="text-6xl font-extrabold mt-3 text-primary">
@@ -55,21 +55,18 @@ const Header = () => {
           </h1>
           <h3 className="text-3xl font-bold mt-4">{headerData.description}</h3>
         </div>
-        <p className="text-3xl font-medium">{headerData.text}</p>
+        <p className="text-2xl font-medium py-12">{headerData.text}</p>
         <Button primary>Zobacz oferty</Button>
       </div>
-      <div
-        className="absolute left-0 top-0 w-full overflow-hidden md:overflow-visible flex justify-center
-       md:block md:relative h-screen md:w-[90rem] xl:w-[70rem] "
-      >
-        {image && (
+      {image && (
+        <div className="absolute bottom-0 max-h-full max-w-[30rem] flex lg:relative">
           <GatsbyImage
-            className="block absolute max-w-[35rem] right-0 bottom-0 drop-shadow-[0_0_60px_rgb(219,39,119)]"
+            className=" drop-shadow-[0_0_60px_rgb(219,39,119)]"
             image={image}
             alt="MrsMatChem's portrait"
           />
-        )}
-      </div>
+        </div>
+      )}
     </section>
   );
 };
