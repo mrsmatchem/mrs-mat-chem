a48c78996001f424a99170b9e8aa006df7de7514
kysniw <kuba.winsyk.forgames@gmail.com>
2024-07-11 20:14:23 +0200

    Fixed offers view

--- src/components/sections/offers/offer.tsx
+++ src/components/sections/offers/offer.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { OfferDataProps } from ".";
 
 const Offer = ({ data }: { data: OfferDataProps }) => {
+  console.log(data);
   return (
     <div
-      className="grow shrink basis-[30rem] flex flex-col justify-between bg-[rgb(9_7_66)]
-   rounded-3xl text-center overflow-hidden border-[0.2rem] border-solid border-primary duration-500
+      className="grow shrink basis-[25rem] flex flex-col justify-between bg-[rgb(9_7_66)]
+   rounded-3xl overflow-hidden border-[0.2rem] border-solid border-primary duration-500
     hover:scale-105"
     >
       <div className="pt-12 pl-8 pr-8">
-        <h3 className="text-4xl">{data.title}</h3>
-        <ul className="text-2xl mt-8 text-left">
+        <h3 className="text-3xl font-bold text-center">{data.title}</h3>
+        <ul className="text-xl mt-8 text-left">
           {data.points.map((point, index) => (
             <li key={index} className="mt-4 mr-8">
               {point}
@@ -19,20 +20,24 @@ const Offer = ({ data }: { data: OfferDataProps }) => {
         </ul>
         <ul className="text-3xl mt-12 mb-8 text-left">
           {data.bonuses.map((bonus, index) => (
-            <li key={index} className="list-none">
-              <h5 className="font-bold text-primary">BONUS {index + 1}:</h5>
-              <p className="mt-3 font-semibold">{bonus}</p>
+            <li key={index} className="mt-3">
+              <h5 className="font-bold text-primary text-2xl">
+                BONUS {index + 1}:
+              </h5>
+              <p className="font-semibold text-xl">{bonus}</p>
             </li>
           ))}
         </ul>
       </div>
       <div className="relative flex min-h-40 flex-col justify-center py-5 px-7 bg-primary">
-        <h5 className="line-through decoration-secondary decoration-2 text-3xl">
-          {data.old_price}
-        </h5>
-        <p className="mt-8 text-3xl">Tylko teraz</p>
+        {data.old_price !== "" && (
+          <h5 className="line-through decoration-secondary decoration-2 text-3xl">
+            {data.old_price}
+          </h5>
+        )}
+        <p className="mt-8 text-2xl text-center">Tylko teraz</p>
         <div className="flex justify-evenly gap-4">
-          <h4 className="block text-4xl">{data.new_price}&nbsp;zł</h4>
+          <h4 className="block text-3xl font-bold">{data.new_price}&nbsp;zł</h4>
         </div>
       </div>
     </div>
